369dbd8c67589c5f472acc6a48c70156bc2f7c80
tymyevhenmpw <tymyevhen@gmail.com>
2025-03-03 21:16:15 +0100

    first commit

---
+++ constants/store.js
@@ -0,0 +1,7 @@
+export const Store = {
+    name: "Планета бажання",
+    currency: "UAH",
+    currency_sign: "₴",
+    domain: "https://fo-scandinavia.vercel.app",
+    database: "test"
+}
---
+++ lib/mongoose.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { Store } from "@/constants/store.js";
+
+let isConnected = false;
+
+export async function connectToDB() {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) {
+    console.log("MONGODB_URL not found");
+    return;
+  }
+
+  if (isConnected) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+
+  try {
+    const uri = `${process.env.MONGODB_URL}${Store.database}?retryWrites=true&w=majority&appName=PlanetaBazannia`;
+    await mongoose.connect(uri);
+
+    console.log(uri)
+    isConnected = true;
+    console.log(`Connected to MongoDB database: ${Store.database}`);
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
+}
